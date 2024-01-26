63bfe8697d4dc83eb2d73435c10402a439342fb4
npex42 <npex42@gmail.com>
2024-01-26 20:28:14 +0000

    Started Parser

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -1,6 +1,9 @@
 package ast
 
-import "tinylang/pkg/token"
+import (
+	"bytes"
+	"tinylang/pkg/token"
+)
 
 type Node interface {
 	String() string
@@ -16,9 +19,28 @@ type Expression interface {
 	expr()
 }
 
+type Program struct {
+	Statements []Statement
+}
+
 type PrintStmt struct {
 	Token token.Token 	`json:"token"`
 	Value Expression	`json:"value"`
+}
+
+func (stmt *PrintStmt) stmt() {}
+func (stmt *PrintStmt) String() string { 
+	var s bytes.Buffer
+
+	s.WriteString("println")
+
+	if (stmt.Value != nil) {
+		s.WriteString(" " + stmt.Value.String())
+	}
+
+	s.WriteString(";")
+
+	return s.String()
 }
 
 
--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -1,5 +1,38 @@
 package parser
 
+import (
+	"tinylang/pkg/lexer"
+	"tinylang/pkg/token"
+)
+
 type Parser struct {
-	
+	CurrToken	token.Token
+	NextToken	token.Token
+
+	l			*lexer.Lexer
 }
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l: l,
+	}
+
+	p.pop()
+	p.pop()
+
+	return p
+}
+
+func (p *Parser) matchCurrent(tt token.TokenType) bool {
+	return p.CurrToken.Type == tt;
+}
+
+func (p *Parser) matchNext(tt token.TokenType) bool {
+	return p.NextToken.Type == tt;
+}
+
+func (p *Parser) pop() {
+	p.CurrToken = p.NextToken
+	p.NextToken = p.l.NextToken()
+}
+
